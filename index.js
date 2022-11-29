e3a9cf45220b74754e44c86aef55e3bfc2e8a129
Ignacio Zamo <ignaciozamo1@gmail.com>
2022-11-29 19:34:01 -0300

    se borra codigo comentado que no iba

--- index.js
+++ index.js
@@ -2,52 +2,6 @@ let ingreseNombre = prompt("Ingese su nombre, por favor");
 let saludo = `Bienvenido ${ingreseNombre} !`;
 alert (saludo);
 
-
-
-
-// let resultado = 0;
-// let prod1 = Number(prompt("Seleccione producto a adquirir (maximo 4): 1: Cable USB $500, 2: Cargador $800 , 3: Blindex $400, 4: Carcaza $1000, 5: bateria $2500, 6: Terminar"));
-
-// for (let i = 0; i<=4; i++) {
-//     if (prod1 === 1 ) {
-//         resultado = resultado + 500;
-//             alert(`Producto ${i} ingresado`);
-//     } else if (prod === 2) {
-//     resultado = resultado + 800;
-//     alert(`Producto ${i} ingresado`);
-//     } else if (prod === 3) {
-//         resultado = resultado + 400;
-//         alert(`Producto ${i} ingresado`);
-            
-            
-//     } else if (prod === 4) {
-//         resultado = resultado + 1000;
-//         alert(`Producto ${i} ingresado`);
-                
-                
-//     } else if (prod === 5) {
-//         resultado = resultado + 2500;
-//         alert(`Producto ${i} ingresado`);
-                    
-                    
-//     } else if (prod === 6) {
-//         i = i+5
-//         alert (`Total a abonar: ${resultado}`);
-//     } 
-//     let prod1 = Number(prompt("Seleccione producto a adquirir (maximo 4): 1: Cable USB $500, 2: Cargador $800 , 3: Blindex $400, 4: Carcaza $1000, 5: bateria $2500, 6: Terminar"));
-//   }
-// alert (`total a pagar será de: $${resultado}`);
-
-
-
-
-// const cableUSB = 500;
-// const cargador = 800;
-// const blindex = 400;
-// const carcaza = 1000;
-// const bateria = 2500;
-
-
 alert ("Seleccione 3 productos a adquirir y la cantidad de cada uno, por favor");
 let seleccionarProductos = Number(prompt("1-Cable USB $500, 2-Cargador $800, 3-Blindex $400, 4-Carcaza $1000 5-Bateria $2500 0-Salir"));
 let Cantidad;
